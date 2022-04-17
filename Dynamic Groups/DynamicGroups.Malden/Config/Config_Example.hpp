bcca5960c72dbb3961712c719daadbd834c996e4
dom <dom_thomas@hotmail.co.uk>
2022-04-17 10:20:51 +0100

    Improve and add custom variables to example config

--- Dynamic Groups/DynamicGroups.Malden/Config/Config_Example.hpp
+++ Dynamic Groups/DynamicGroups.Malden/Config/Config_Example.hpp
@@ -2,11 +2,14 @@
 	Faction: Example
 	Author: Dom
 */
-class nameOfRole { //this class is used when defining which roles are in what group
+class roleClassname { //this class is used when defining which roles are in what group
 	name = "Example Role"; //this is the name of the role as shown in the menu, can either be plain string or found in a stringtable using $ e.g $STR_B_DIVER_F0
 	rank = "Private"; //can be one of the following "Private", "Corporal", "Sergeant", "Lieutenant", "Captain", "Major", "Colonel"
 	description = $STR_DT_Diver_Description; //this is the description of the role shown in the menu, can be plain string or found in stringtable
 	traits[] = { //these are specific traits to the unit in the following format {"trait",value,isCustom}}, more information here: https://community.bistudio.com/wiki/setUnitTrait
+
+	};
+	customVariables[] = { //these variables are set on the player object once the role is taken
 
 	};
 	icon = "a3\ui_f\data\map\vehicleicons\iconMan_ca.paa"; //this icon is shown on the menu when picking a role
